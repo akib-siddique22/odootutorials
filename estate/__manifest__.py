0781e527aec55ceae1f28937513a059757b1d51d
Akib Siddique <Akibs2001@gmail.com>
2025-01-24 05:30:06 +0800

    added path to security and data files

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,4 +4,11 @@
 {
     'name': 'Real Estate',
     'application': True,
+
+    'data': [
+        'security/ir.model.access.csv',
+
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
 }
