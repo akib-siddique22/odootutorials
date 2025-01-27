0c358c5fc3a899f7256362756b0442afd7e4d528
Akib Siddique <Akibs2001@gmail.com>
2025-01-28 03:05:01 +0800

    added computes for deadline and methods for accepting and rejecting offers

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import fields, models, api
 #from dateutil.relativedelta import relativedelta
+from datetime import datetime, timedelta
+from . import estate_property
 
 
 class EstatePropertyOffer(models.Model):
@@ -10,10 +12,43 @@ class EstatePropertyOffer(models.Model):
     _description = "Property Offer"
 
     price = fields.Float()
+    create_date = fields.Datetime(default=fields.Datetime.now)
     status = fields.Selection(
         string='Status',
         selection=[('accepted', 'Accepted'), ('refused', 'Refused')],
         help="Offer Decision",
         copy = False)
+    validity = fields.Integer(default=7, inverse="_inverse_deadline", string="Validity (days)")
+    date_deadline = fields.Date(compute="_compute_deadline", string="Deadline")
     partner_id = fields.Many2one("res.partner", string="Partner", required=True)
     property_id = fields.Many2one("estate.property", string="Property ID", required=True)
+
+    @api.depends('create_date', 'validity')
+    def _compute_deadline(self):
+        for line in self:
+            line.date_deadline = line.create_date.date() + timedelta(days=line.validity)
+    
+    @api.depends('create_date', 'date_deadline')
+    def _inverse_deadline(self):
+        for line in self:
+            line.validity = (line.date_deadline - line.create_date.date()).days
+
+    def action_accept(self):
+        for record in self:
+            if self.property_id.sell_accepted == False:
+                self.property_id.sell_accepted = True
+                self.property_id.selling_price = self.price
+                self.property_id.buyer = self.partner_id.id
+                record.status = 'accepted'
+
+    def action_reject(self):
+        for record in self:
+            if self.status == 'accepted':
+                self.property_id.sell_accepted = False
+                self.property_id.selling_price = 0
+                self.property_id.buyer = None
+                record.status = 'refused'
+            else:
+                record.status = 'refused'
+
+        
