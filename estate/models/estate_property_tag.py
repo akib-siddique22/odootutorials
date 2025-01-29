dd80502f44eeeff95798230f1e6a03fa0fe18e94
Akib Siddique <Akibs2001@gmail.com>
2025-01-30 05:19:12 +0800

    can change color

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -5,8 +5,10 @@ from odoo import fields, models
 class EstatePropertyTag(models.Model):
     _name = "estate.property.tag"
     _description = "Property Tag"
+    _order = "name"
 
     name = fields.Char(required=True)
+    color = fields.Integer(string="Color")
 
     _sql_constraints = [
         ('check_tag_name', 'unique(name)',
